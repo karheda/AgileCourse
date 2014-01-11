f210d8ace620a16615766a34d68713f609351604
Carlos Hernandez <carlos.hernandez@akamon.com>
2014-01-11 14:35:15 +0100

    moved test files to right folder

--- src/Dave.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Carlos
- * Date: 11/01/14
- * Time: 12:37
- * To change this template use File | Settings | File Templates.
- */
-public interface Dave {
-}
